8a13ecbb7d856043c050c504425623df14a95c0e
Miracle Okafor <miracleokafor@christians-MacBook-Pro.local>
2019-11-21 00:02:29 +0100

    added overview page

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,47 +1,117 @@
 import React, { Component } from "react";
-// import ChartistGraph from "react-chartist";
+import { Line } from "react-chartjs-2";
 import { Grid, 
-  // Row, 
-  // Col 
+  Row, 
+  Col 
 } from "react-bootstrap";
 
-// import { Card } from "components/Card/Card.jsx";
-// import { StatsCard } from "components/StatsCard/StatsCard.jsx";
-// import { Tasks } from "components/Tasks/Tasks.jsx";
-// import {
-//   dataPie,
-//   legendPie,
-//   dataSales,
-//   optionsSales,
-//   responsiveSales,
-//   legendSales,
-//   dataBar,
-//   optionsBar,
-//   responsiveBar,
-//   legendBar
-// } from "variables/Variables.jsx";
+import { Card } from "components/Card/Card.jsx";
+import { StatsCard } from "components/StatsCard/StatsCard.jsx";
 
 class Overview extends Component {
-  createLegend(json) {
-    var legend = [];
-    for (var i = 0; i < json["names"].length; i++) {
-      var type = "fa fa-circle text-" + json["types"][i];
-      legend.push(<i className={type} key={i} />);
-      legend.push(" ");
-      legend.push(json["names"][i]);
-    }
-    return legend;
-  }
+  state = {
+    loading: true,
+    currentScreen: "pko",
+    currentView: "dailySales",
+    revenue_data: {
+      labels: ["Nov 1st","Nov 2nd","Nov 3rd","Nov 4th","Nov 5th","Nov 6th"],
+      datasets: [
+        {
+          yAxisID: "A",
+          label: "Accumulated Daily Revenue",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [10000,25000,27000,35000,48000,69000]
+        }
+      ]
+    },
+    
+    startDate: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000),
+    endDate: new Date(),
+    currentDateFilter: "currentWeek",
+    graphView: "day",
+    salesCyclesAvg: "N/A",
+    currency: "naira",
+  };
+
   render() {
+
+    const options = { maintainAspectRatio: false, responsive: true,
+      tooltips : {
+        mode: "label",
+        callbacks: {
+          label: function(tooltipItem, data) {
+            const key = data.datasets[tooltipItem.datasetIndex].label;
+            const yAxis = data.datasets[tooltipItem.datasetIndex].yAxisID;
+            const val =
+              data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+            if (val && yAxis === "A") return key + ": " +val.toLocaleString() +" tons";
+            if (val && yAxis === "B") return key + ` :  ₦` + val.toLocaleString();
+            // if (val && yAxis === "B") return key + ` : ${currency === "naira" ? "₦":"$"}` + val.toLocaleString();
+          }
+        }
+      },
+      scales:{
+        yAxes :[
+          {
+            type: "linear",
+            display: true,
+            position: "left",
+            id: "A",
+            scaleLabel: {
+              display: true,
+              labelString: ""
+            },
+            ticks: {
+              callback: value => ` ₦ ` + value.toLocaleString()
+              // callback: value => ` ${currency === "naira" ? "₦":"$"} ` + value.toLocaleString()
+            }
+          }
+        ]
+      }
+    };
     return (
       <div className="content">
         <Grid fluid>
-          {/* <Row>
+          <Row>
             <Col lg={3} sm={6}>
               <StatsCard
                 bigIcon={<i className="pe-7s-server text-warning" />}
-                statsText="Capacity"
-                statsValue="105GB"
+                statsText="Utilization Rate"
+                statsValue="68%"
+                statsIcon={<i className="pe-7s-server" />}
+                statsIconText="Utilization Rate"
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="pe-7s-graph1 text-danger" />}
+                statsText="Downtime"
+                statsValue="8%"
+                statsIcon={<i className="fa fa-clock-o" />}
+                statsIconText="In the last hour"
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="pe-7s-note2 text-info" />}
+                statsText="Groos Margin"
+                statsValue="53%"
                 statsIcon={<i className="fa fa-refresh" />}
                 statsIconText="Updated now"
               />
@@ -49,55 +119,34 @@ class Overview extends Component {
             <Col lg={3} sm={6}>
               <StatsCard
                 bigIcon={<i className="pe-7s-wallet text-success" />}
-                statsText="Revenue"
-                statsValue="$1,345"
+                statsText="Total Revenue"
+                statsValue="₦95,450"
                 statsIcon={<i className="fa fa-calendar-o" />}
                 statsIconText="Last day"
               />
             </Col>
-            <Col lg={3} sm={6}>
-              <StatsCard
-                bigIcon={<i className="pe-7s-graph1 text-danger" />}
-                statsText="Errors"
-                statsValue="23"
-                statsIcon={<i className="fa fa-clock-o" />}
-                statsIconText="In the last hour"
-              />
-            </Col>
-            <Col lg={3} sm={6}>
-              <StatsCard
-                bigIcon={<i className="fa fa-twitter text-info" />}
-                statsText="Followers"
-                statsValue="+45"
-                statsIcon={<i className="fa fa-refresh" />}
-                statsIconText="Updated now"
-              />
-            </Col>
           </Row>
           <Row>
-            <Col md={8}>
+            <Col md={12}>
               <Card
                 statsIcon="fa fa-history"
                 id="chartHours"
-                title="Users Behavior"
-                category="24 Hours performance"
-                stats="Updated 3 minutes ago"
+                title="Revenue"
+                category="Accumulated Revenue"
+                stats="Revenue Chart"
                 content={
                   <div className="ct-chart">
-                    <ChartistGraph
-                      data={dataSales}
-                      type="Line"
-                      options={optionsSales}
-                      responsiveOptions={responsiveSales}
+                    <Line
+                      height={400}
+                      width={800}
+                      data={this.state.revenue_data}
+                      options={options}
                     />
                   </div>
                 }
-                legend={
-                  <div className="legend">{this.createLegend(legendSales)}</div>
-                }
               />
             </Col>
-            <Col md={4}>
+            {/* <Col md={4}>
               <Card
                 statsIcon="fa fa-clock-o"
                 title="Email Statistics"
@@ -115,10 +164,10 @@ class Overview extends Component {
                   <div className="legend">{this.createLegend(legendPie)}</div>
                 }
               />
-            </Col>
+            </Col> */}
           </Row>
 
-          <Row>
+          {/* <Row>
             <Col md={6}>
               <Card
                 id="chartActivity"
